5b0d80690e7c023cde07683ae666b2e98dd8b7d4
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 18:06:07 +0900

    .keysと.valuesを追加

--- fruits.py
+++ fruits.py
@@ -1,4 +1,18 @@
-fruits = {"りんご": "Apple",
-          "ぶどう": "Grape",
-          "パイナップル": "Pineapple"
-}
+fruits_dict = {"りんご": "Apple",
+               "ぶどう": "Grape",
+               "みかん": "Orange"
+               }
+
+# print(fruits_dict["ぶどう"])
+
+# print(fruits.keys())
+
+
+for jp_name in fruits_dict.keys():
+    print(jp_name)
+
+for en_name in fruits_dict.values():
+    print(en_name)
+
+for jp_name, en_name in fruits_dict.items():
+    print(f"{jp_name}は英語で{en_name}")
